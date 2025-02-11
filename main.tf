524bab6bcd79a8ab5084b4bf7439f43d7c6f3089
Sumanth17-git <sumanth.suman17@gmail.com>
2025-02-11 19:42:20 +0530

    update

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
   }
 }
 provider "google" {
-  project     = "aiops-447509"  # Replace with your GCP Project ID
-  region      = "us-east4"
+  project = "aiops-447509" # Replace with your GCP Project ID
+  region  = "us-east4"
 }
 
 resource "google_storage_bucket" "my-bucket" {
   name          = "sumi-simple-bucket" # Bucket name must be globally unique
-  location      = "US" # Multi-region location
-  force_destroy = true # Allows the bucket to be deleted even if it contains objects
+  location      = "US"                 # Multi-region location
+  force_destroy = true                 # Allows the bucket to be deleted even if it contains objects
 }
